b3ec9a055b2852498cd1e0b4bbf9a66fb25aa88d
Dominik Wittenberg <dwittenberg@np4.net>
2020-06-24 12:52:53 +0200

    Must remove AllInOneFile, was to large. with GUI.

--- GenerateExe.ps1
+++ GenerateExe.ps1
@@ -1,2 +1,2 @@
-dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -o ./bin/ --self-contained
+dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true -o ./bin/ --self-contained
 del ./bin/*.pdb
